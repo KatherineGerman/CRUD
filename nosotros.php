fdf0be01ccd3537b581b96538e18846094a661fd
kathrd <katherine03gt@gmail.com>
2021-07-06 17:19:44 -0400

    Enmaquetacion de proyecto

---
+++ nosotros.php
@@ -0,0 +1,12 @@
+<?php include("template/header.php");?>
+
+<div class="jumbotron">
+        <h1 class="display-3">Jumbo heading</h1>
+        <p class="lead">Holaa somos un grupo de desarrolladores de paginas web. </p>
+        <hr class="my-2">
+    
+    </div>
+
+
+
+<?php include("template/footer.php");?>
---
+++ productos.php
@@ -0,0 +1,62 @@
+<?php include("template/header.php");?>
+
+<div class="col-md-3">
+
+    <div class="card">
+
+        <img class="card-img-top" src="holder.js/100x180/" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Libro PHP</h4>
+           <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+        </div>
+
+    </div>
+
+</div>
+
+<div class="col-md-3">
+
+    <div class="card">
+
+        <img class="card-img-top" src="holder.js/100x180/" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Libro PHP</h4>
+           <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+        </div>
+
+    </div>
+
+</div>
+
+<div class="col-md-3">
+
+    <div class="card">
+
+        <img class="card-img-top" src="holder.js/100x180/" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Libro PHP</h4>
+           <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+        </div>
+
+    </div>
+
+</div>
+
+<div class="col-md-3">
+
+    <div class="card">
+
+        <img class="card-img-top" src="holder.js/100x180/" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Libro PHP</h4>
+           <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+        </div>
+
+    </div>
+
+</div>
+
+
+
+
+<?php include("template/footer.php");?>
