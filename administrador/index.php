9e0812220b449580aa04f89a07cfcf2f45f8a788
kathrd <katherine03gt@gmail.com>
2021-07-09 14:13:46 -0400

    Seccion de Adminitrador

--- administrador/index.php
+++ administrador/index.php
@@ -1,13 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administrador del sitio web</title>
-</head>
-<body>
+<?php
 
-    Login
-</body>
+if($_POST){
+header('Location:inicio.php');
+
+}
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Administraor</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+   
+   <div class="container">
+       <div class="row">
+
+       <div class="col-md-4">
+           
+       </div>
+           <div class="col-md-4">
+
+           <br /> <br /> <br />
+           <div class="card">
+               <div class="card-header">
+                   Login
+               </div>
+
+               <div class="card-body">
+
+               <form method="POST">
+
+               <div class = "form-group">
+               <label >Usuario</label>
+               <input type="text" class="form-control" name="usuario" aria-describedby="emailHelp" placeholder="Ingresa usuario">
+              
+               </div>
+
+               <div class="form-group">
+               <label >Contraseña</label>
+               <input type="password" class="form-control" name="contraseña" placeholder="Contraseña">
+               </div>
+
+            
+               <button type="submit" class="btn btn-primary">Ingresar </button>
+              
+               </form>
+               
+               
+                  
+               </div>
+               
+           </div>
+               
+           </div>
+           
+       </div>
+   </div>
+  </body>
 </html>
