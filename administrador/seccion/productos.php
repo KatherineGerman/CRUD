2c6c7a1ba4f6449372553cdb8bd181054ac8cea1
kathrd <katherine03gt@gmail.com>
2021-07-09 22:20:02 -0400

    Preparacion de la seccion de CRUD

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -1,1 +1,16 @@
-Libros 
+<?php include("../template/header.php"); ?>
+
+<div class="col-md-5">
+   Formulario de agregar libros
+    
+</div>
+
+<div class="col-md-7">
+Tabla de libros(donde se van a mostrar)
+
+    
+</div>
+
+
+
+<?php include("../template/footer.php"); ?>
